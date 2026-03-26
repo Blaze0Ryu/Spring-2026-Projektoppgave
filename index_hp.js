cd545df1ae29de6007787dfd1c59db0301fb7e60
Blaze0Ryu <rosa.lin.reisacherr@gmx.de>
2026-03-26 11:23:22 +0100

    collapsible + click button wip

--- index_hp.js
+++ index_hp.js
@@ -1,4 +1,4 @@
-/*Alert pop up box*/
+/*Alert pop up box
 function myFunction() {
   var txt;
   if (
@@ -9,4 +9,22 @@ function myFunction() {
     txt = "You have pressed cancel.";
   }
   document.getElementById("demo").innerHTML = txt;
-}
+}*/
+
+/**/
+/**/
+/*Collabsible containers ffrom the KKantine.js & PrototypeHjemside_Game.js*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++)
+  coll[1].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var content = this.nextElementSiblings;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
